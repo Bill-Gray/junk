67a9a35f33c2583a7247d82453d6256d61f82977
Bill-Gray <pluto@projectpluto.com>
2018-12-01 18:05:31 -0500

    Cleaned up and commented the code.  Added macros to suppress warnings about intentionally unused parameters.

--- kbhit2.c
+++ kbhit2.c
@@ -1,15 +1,21 @@
-/* Based in part on... */
-/* http://flipcode.com/archives/_kbhit_for_Linux.shtml */
+/* Based on the "general" version of kbhit() at
+
+http://flipcode.com/archives/_kbhit_for_Linux.shtml
+
+   Note that 'echo' is turned off,  and the terminal is reset to
+its original state when we exit.  Unless your program aborts.
+Some use of atexit( ) may be appropriate.       */
 
 #include <sys/select.h>
 #include <stdio.h>
 #include <termios.h>
 
+#define STDIN  0
+
 int kbhit( void)
 {
    struct timeval timeout;
    fd_set rdset;
-   const int STDIN = 0;
 
    FD_ZERO( &rdset);
    FD_SET( STDIN, &rdset);
@@ -18,20 +24,25 @@ int kbhit( void)
    return( select( STDIN + 1, &rdset, NULL, NULL, &timeout));
 }
 
+/* The following reassures the compiler that it need not warn us
+that some parameters (here,  argc and argv) are not used. */
+
+#define INTENTIONALLY_UNUSED_PARAMETER( param) (void)(param)
+
 int main( const int argc, const char **argv)
 {
-   const int STDIN = 0;
    int c = 0;
    // Use termios to turn off line buffering
    struct termios term, old_term;
 
-   tcgetattr(STDIN, &term);
+   INTENTIONALLY_UNUSED_PARAMETER( argv);
+   INTENTIONALLY_UNUSED_PARAMETER( argc);
+   tcgetattr( STDIN, &term);
    old_term = term;
    term.c_lflag &= ~(ICANON | ECHO);
-// term.c_lflag |= ECHO;
-   tcsetattr(STDIN, TCSANOW, &term);
+   tcsetattr( STDIN, TCSANOW, &term);
 
-   setbuf(stdin, NULL);
+   setbuf( stdin, NULL);
    setbuf( stdout, NULL);
    while( c != 'q')
       {
